82c0d7791d6896c18fd3a8578c3d12b9d7f1e78d
subam kumar <subamkumar1997@gmail.com>
2021-03-11 22:09:19 +0530

    changing the metadata for the RPA system

--- metadata.go
+++ metadata.go
@@ -4,43 +4,42 @@ import "github.com/project-flogo/core/data/coerce"
 
 type Settings struct{
 	URL string `md:"API Gateway URL,required"`
-	Radio string `md:"TestBoolean,required"`
 }
 
 type Input struct {
-	ProcessURL 		string 					`md:"process_url"`
-	ProcessorType 	string 					`md:"processor_type"`
-	Parameters 		map[string]interface{} 	`md:"parameters"`
-	Log				map[string]interface{}	`md:"log"`
-	PostProcesses 	[]interface{} 			`md:"post_processes"`
+	ProcessorType 		string 					`md:"processType"`
+	getById 			string 					`md:"getId"`
+	CollectionQueryId 	string	 				`md:"filterByCollectionId"`
+	LocationQueryId		string					`md:"filterByLocationId"`
+	ActivityQueryId 	string		 			`md:"filterByActivityId"`
 }
 
 func (r *Input) FromMap(values map[string]interface{}) error {
-	processURLVal, _ := coerce.ToString(values["process_url"])
-	r.ProcessURL = processURLVal
-
-	processTypeVal, _ := coerce.ToString(values["processor_type"])
+	processTypeVal, _ := coerce.ToString(values["processType"])
 	r.ProcessorType = processTypeVal
 
-	parametersVal, _ := coerce.ToObject(values["parameters"])
-	r.Parameters = parametersVal
+	pathGetId, _ := coerce.ToString(values["getId"])
+	r.getById = pathGetId
 
-	logVal, _ := coerce.ToObject(values["log"])
-	r.Log = logVal
+	collectionFilter, _ := coerce.ToString(values["filterByCollectionId"])
+	r.CollectionQueryId = collectionFilter
 
-	postProcessesVal, _ := coerce.ToArray(values["post_processes"])
-	r.PostProcesses = postProcessesVal
+	locationFilter, _ := coerce.ToString(values["filterByLocationId"])
+	r.LocationQueryId = locationFilter
+
+	activityFilter, _ := coerce.ToString(values["filterByActivityId"])
+	r.ActivityQueryId = activityFilter
 
 	return nil
 }
 
 func (r *Input) ToMap() map[string]interface{} {
 	return map[string]interface{}{
-		"process_url": r.ProcessURL,
-		"processor_type": r.ProcessorType,
-		"parameters": r.Parameters,
-		"log": r.Log,
-		"post_processes": r.PostProcesses,
+		"processType": r.ProcessorType,
+		"getId": r.getById,
+		"filterByCollectionId": r.CollectionQueryId,
+		"filterByLocationId": r.LocationQueryId,
+		"filterByActivityId": r.ActivityQueryId,
 	}
 }
 
